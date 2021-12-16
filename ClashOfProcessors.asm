fe71b4cb7b3a2ea37aa3978e2574aa3d54235691
Bemoi Erian <bemoi.erian@gmail.com>
2021-12-16 20:01:37 +0200

    Added the conept of reading input from the keyboard, Made the gun not override the color it passes on

--- ClashOfProcessors.asm
+++ ClashOfProcessors.asm
@@ -24,7 +24,7 @@ Rectangel MACRO x,y
      cmp dx,di
      jnz outer
      ;-----------
-ENDM
+ENDM Rectangel
 DrawRegisters MACRO
     mov Rectanglexpos,10
     mov Rectangleypos,20
@@ -39,7 +39,10 @@ DrawRegisters MACRO
         dec counterDrawRegisters
         cmp counterDrawRegisters,0
     jnz Rectangel8
-ENDM
+    mov Rectanglexpos, 10
+    mov Rectangleypos, 10
+    mov counterDrawRegisters, 4
+ENDM DrawRegisters
 
 .DATA
 ;------------------Variables for registers drawing----------------------
@@ -55,56 +58,65 @@ gunNewY dw 50
 MAIN PROC FAR
     MOV AX, @DATA
     MOV DS, AX
-
+    
     mov ah,0   ;graphics mode
     mov al,13h
     int 10h
-    DrawRegisters
-    call MoveGun
+    
+    UserNames:
 
+
+
+    MainScreen:
+
+    ;Main Game Loop
+    Game:
+  
+        DrawRegisters
+
+        ;Read Keyboard input
+        mov ah, 1
+        int 16h
+        jz Game   ;if no key is pressed, go to other functions like flying objects, etc. -----------to be changed--------------
+        mov ah, 0
+        int 16h
+        ;AL contains ascii of key pressed
+        ;-------------------------------------------------INPORTANT NOTE-------------------------------------------------------
+        ;DON'T CALL ANY FUNCTION HERE THAT CHANGES THE VALUE OF AX,
+        ;IF YOU WANT TO USE AX, PUSH IT IN REG THEN POP WHEN YOU FINISH TO RESTORE ITS VALUE 
+        Gun:
+            ;right arrow
+            right:
+                cmp ah, 4Dh ;compare key code with right key code
+                jnz left    ;if the key is not right, jump to next check
+                add gunNewX, 3  ;if the key is right, move the gun 3 pixels to the right
+                jmp EndGun
+            ;left arrow    
+            left:
+                cmp ah, 4Bh
+                jnz up
+                sub gunNewX, 3
+                jmp EndGun
+            ;up arrow
+            up:
+                cmp ah, 48h
+                jnz down
+                sub gunNewY, 3
+                jmp EndGun
+            ;down arrow
+            down:
+                cmp ah, 50h
+                jnz EndGun
+                add gunNewY, 3
+        EndGun:     
+            call DrawGun
+
+        ;Exit game if key if F3
+        cmp al, 13h
+        jz MainScreen
+        jmp Game
 HLT
 MAIN ENDP
-
-
-;Reads the input from user (left,right,up,down arrows) and moves the gun
-MoveGun PROC
-    pusha
-    loop1:
-        mov ah,0
-        int 16h   ;Get key pressed (Wait for a key-AH:scancode,AL:ASCII)
-        ;right arrow
-        right:
-            cmp ah, 4Dh ;compare key code with right key code
-            jnz left    ;if the key is not right, jump to next check
-            add gunNewX, 3  ;if the key is right, move the gun 3 pixels to the right
-            call DrawGun    ;draw the new gun
-            jmp loop1       ;loop to check the next input
-        ;left arrow    
-        left:
-            cmp ah, 4Bh
-            jnz up
-            sub gunNewX, 3
-            call DrawGun
-            jmp loop1
-        ;up arrow
-        up:
-            cmp ah, 48h
-            jnz down
-            sub gunNewY, 3
-            call DrawGun
-            jmp loop1
-        ;down arrow
-        down:
-            cmp ah, 50h
-            jnz esc1
-            add gunNewY, 3
-            call DrawGun
-            jmp loop1          
-    
-    esc1:
-    popa
-    ret
-MoveGun ENDP
 
 
 ;Draws gun at the new position at gunNewX, gunNewY and stores the previous position in gunPrevX, gunPrevY
@@ -117,23 +129,6 @@ DrawGun proc
     mov dx,gunPrevY   ;Row       
     mov al,0h        ;Pixel color
     mov ah,0ch       ;Draw Pixel Command
-    ;loop to delete the previous gun (Delete here means draw a black box on it)
-    outer1: ;outer to increment rows
-        mov cx, gunPrevX ;X position of previous gun
-        mov bl, 0 ;inner counter
-        inc dx    ;increment row
-        inc bh    ;outer counter
-        cmp bh,3  ;if draw 3 rows then then the gun is deleted
-        jz exit1
-    
-        inner1:  ;to draw lines
-        int 10h  ;draw pixel
-        inc cx   ;inc column
-        inc bl   ;inc counter
-        cmp bl,3 ;if you draw 3 columns jump to outer 
-        jnz inner1
-        jz outer1
-    exit1:
     ;Draw pixel
     mov bx, 0
     mov cx,gunNewX   ;Column
