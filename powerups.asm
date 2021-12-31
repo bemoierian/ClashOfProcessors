92e53737c5ee191975e5cce3bcb2d993d4ff3d8f
peter_atef@stud.cu.edu.eg <peter_atef@stud.cu.edu.eg>
2021-12-31 18:01:00 +0200

    ali b3do

--- powerups.asm
+++ powerups.asm
@@ -1,16 +1,29 @@
-PUBLIC changeForbidden
-PUBLIC forbidden
+PUBLIC changeForbidden1
+PUBLIC forbidden1
+PUBLIC changeForbidden2
+PUBLIC forbidden2
+
 .model small
 .data
-forbidden db ?
+forbidden1 db 0
+forbidden2 db 0
 .code
 
-changeForbidden PROC far
+changeForbidden1 PROC far
 MOV AX,@DATA
 MOV DS,AX
 MOV AH,01H
 INT 21h
-MOV forbidden,AL
+MOV forbidden1,AL
 RET
-changeForbidden ENDP
+changeForbidden1 ENDP
+
+changeForbidden2 PROC far
+MOV AX,@DATA
+MOV DS,AX
+MOV AH,01H
+INT 21h
+MOV forbidden2,AL
+RET
+changeForbidden2 ENDP
 END
